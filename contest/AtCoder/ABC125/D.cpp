b528dd6d8d23eeb23f956531ba58120a3e4b63b0
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2022-09-05 15:00:02 +0100

    Improved ABC 125 D solution

--- contest/AtCoder/ABC125/D.cpp
+++ contest/AtCoder/ABC125/D.cpp
@@ -1,29 +1,24 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+const int INF = 1e9 + 5;
 
 int main() {
 	int N;
 	std::cin >> N;
 	std::vector<int> A(N);
 	long long abs_sum = 0;
-	int abs_min = 1e9;
-	bool has_zero = false;
+	int abs_min = INF;
 	for (int i = 0; i < N; ++i) {
 		std::cin >> A[i];
 		abs_sum += abs(A[i]);
 		abs_min = std::min(abs_min, abs(A[i]));
-		has_zero |= !A[i];
 	}
-	for (int i = 0; i < N - 1; ++i) { // push negative to end
+	for (int i = 0; i < N - 1; ++i) {
 		if (A[i] < 0) {
 			A[i + 1] *= -1;
 		}
 	}
-	if (A.back() > 0 || has_zero) { // can get rid of negative
-		std::cout << abs_sum << std::endl;
-	} else { // push negative to least value
-		std::cout << abs_sum - 2 * abs_min << std::endl;
-	}
+    std::cout << abs_sum - (A.back() < 0 ? abs_min * 2 : 0) << std::endl;
 	return 0;
 }
