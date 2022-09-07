fe77205cee673d6b31d5a8e6a00796b8a48e007d
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2022-09-07 04:07:57 +0100

    Improved Kickstart2020/B solution

--- contest/KickStart/RoundC2020/B.cpp
+++ contest/KickStart/RoundC2020/B.cpp
@@ -4,11 +4,13 @@ using namespace std;
 struct directed_graph {
     vector<vector<int>> adj;
     vector<int> in_degree;
+    vector<bool> present;
     int num_nodes;
 
     directed_graph(int n) : num_nodes(n) {
         adj.assign(num_nodes, {});
         in_degree.assign(num_nodes, 0);
+        present.assign(num_nodes, 0);
     }
 
     void add_edge(int i, int j) {
@@ -18,9 +20,13 @@ struct directed_graph {
 
     pair<bool, vector<int>> topo_sort() {
         vector<int> order;
+        int node_count = 0;
         for (int i = 0; i < num_nodes; ++i) {
-            if (!in_degree[i]) {
-                order.push_back(i);
+            if (present[i]) {
+                node_count++;
+                if (!in_degree[i]) {
+                    order.push_back(i);
+                }
             }
         }
         for (int i = 0; i < (int) order.size(); ++i) {
@@ -31,7 +37,7 @@ struct directed_graph {
                 }
             }
         }
-        return {(int) order.size() == num_nodes, order};
+        return {(int) order.size() == node_count, order};
     }
 };
 
@@ -39,39 +45,22 @@ void solve() {
     int R, C;
     cin >> R >> C;
     vector<string> grid(R);
-    map<char, int> get_id;
-    map<int, char> get_letter;
-    int num_letters = 0;
+    directed_graph graph(26);
     for (int i = 0; i < R; ++i) {
         cin >> grid[i];
-        for (const char& letter : grid[i]) {
-            if (!get_id.count(letter)) {
-                get_id[letter] = num_letters;
-                get_letter[num_letters] = letter;
-                num_letters++;
-            }
-        }
+        for (const char& c: grid[i]) 
+            graph.present[c - 'A'] = true;
     }
-    directed_graph graph(num_letters);
-    auto add_edge = [&](char a, char b) {
-        graph.add_edge(get_id[a], get_id[b]);
-    };
-    for (int i = 0; i + 1 < R; ++i) {
-        for (int j = 0; j < C; ++j) {
-            int above = grid[i][j], below = grid[i + 1][j];
-            if (above != below) {
-                add_edge(below, above); // polyomino below must be built before one above
-            }
-        }
-    }
+    for (int i = 0; i + 1 < R; ++i)
+        for (int j = 0; j < C; ++j)
+            if (grid[i][j] != grid[i + 1][j])
+                graph.add_edge(grid[i + 1][j] - 'A', grid[i][j] - 'A');
     auto [possible, order] = graph.topo_sort();
-    if (possible) {
-        for (const int& node : order) {
-            cout << get_letter[node];
-        }
-    } else {
+    if (possible)
+        for (const int& node : order) 
+            cout << char(node + 'A');
+    else
         cout << -1;
-    }
     cout << '\n';
 }
 
