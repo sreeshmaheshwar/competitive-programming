202ed44c02d9f41978a68da9d7695b844126f641
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2022-09-07 01:17:16 +0100

    Added Fenwick tree with set function

--- implementation/fenwick_tree/fenwick_tree_with_set.cpp
+++ implementation/fenwick_tree/fenwick_tree_with_set.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 template<typename T> struct fenwick_tree {
-    vector<T> data;
+    vector<T> arr, data;
     int n;
 
     fenwick_tree(int _n) : n(_n) {
-        data.assign(n, T(0));
+        arr = data = vector<T>(n, T(0));
     }
  
     T sum(int r) {
@@ -17,50 +17,39 @@ template<typename T> struct fenwick_tree {
     }
  
     T qry(int l, int r) { 
-        // if (l > r) return T(0); // UNCOMMENT IF NEEDED!
         return sum(r) - sum(l - 1); 
     }
  
     void upd_by(int index, T delta) { 
+        arr[index] += delta;
         for (; index < n; index = index | (index + 1))
             data[index] += delta;
     }
+
+    void set(int index, T new_val) {
+        upd_by(index, new_val - arr[index]);
+    }
 };
- 
-/*
-Standard Fenwick Tree
-Usage:
-    fenwick_tree<T> ft(n);
-Notes: 
-    update method uses delta NOT updated value
-*/
 
-
-/*
-Example Main Program:
-(solution to https://cses.fi/problemset/submit/1648/)
-*/
- 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	int n, q; 
 	cin >> n >> q;
-	vector<int> arr(n);
 	fenwick_tree<long long> ft(n);
 	for (int i = 0; i < n; ++i) {
-		cin >> arr[i];
-		ft.upd_by(i, arr[i]);
-	}
+        int a;
+        cin >> a;
+        ft.set(i, a);
+    }
 	while (q--) {
 		int type;
 		cin >> type; 
 		if (type == 1) {
 			int ind, val;
 			cin >> ind >> val;
-			ind--;
-			ft.upd_by(ind, val - arr[ind]);
-			arr[ind] = val;
+            ind--;
+            ft.set(ind - 1, val);
 		} else {
 			int l, r; 
 			cin >> l >> r; 
