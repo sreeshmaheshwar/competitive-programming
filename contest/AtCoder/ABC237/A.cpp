55658cc182f33f25c3c22eb9a759279d99145e61
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2022-11-09 01:13:11 +0000

    Improved Kickstart 2021F/E solution

--- contest/AtCoder/ABC237/A.cpp
+++ contest/AtCoder/ABC237/A.cpp
@@ -3,6 +3,6 @@
 int main() {
 	int64_t n;
 	std::cin >> n;
-	std::cout << (1LL << 31 >= -n && +n < (1LL << 31) ? "Yes" : "No");
+	std::cout << (1LL << 31 >= -n && +n < 1LL << 31 ? "Yes" : "No");
 	return 0;
 }
--- contest/KickStart/RoundF2021/C.cpp
+++ contest/KickStart/RoundF2021/C.cpp
@@ -1,78 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
-const double INF = 1e9;
+
+constexpr double INF = 1e9;
 
 struct point {
-	long long x, y;
+  private:
+    int x, y;
 
-	point() {}
-	point(long long _x, long long _y) : x(_x), y(_y) {}
+    friend istream& operator>>(istream& is, point& p) {
+        return is >> p.x >> p.y;
+    }
 
-	void read() {
-		std::cin >> x >> y;
-	}
+  public:
+    void operator-=(const point& other) {
+        x -= other.x, y -= other.y;
+    }
 
-	point operator-(const point &p) const {
-		return point(x - p.x, y - p.y);
-	}
-	void operator-=(const point &p) {
-		x -= p.x;
-		y -= p.y;
-	}
+    int64_t operator*(const point& other) const {
+        return int64_t(x) * other.y - int64_t(other.x) * y;
+    }
 
-	long long cross_product(const point &p) {
-		return x * p.y - y * p.x;
-	}
-	
-	double distance(const point &p) {
-		return std::hypot(x - p.x, y - p.y);
-	}
+    double distance(const point& other) const {
+        return hypot(x - other.x, y - other.y);
+    }
 };
 
-void solve() {
-	int n; 
-	cin >> n;
-	vector<point> points(n);
-	for (point &star : points) {
-		star.read();
-	}
-	point blue_star;
-	blue_star.read();
-	for (point &star : points) {
-		star -= blue_star;
-	}
-	vector<vector<double>> dist(n, vector<double>(n));
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			if (points[i].cross_product(points[j]) > 0) {
-				dist[i][j] = points[i].distance(points[j]);
-			} else {
-				dist[i][j] = INF;
-			}
-		}
-	}
-	for (int k = 0; k < n; ++k) {
-		for (int i = 0; i < n; ++i) {
-			for (int j = 0; j < n; ++j) {
-				dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-			}
-		}
-	}
-	double ans = INF;
-	for (int i = 0; i < n; ++i) {
-		ans = min(ans, dist[i][i]);
-	}
-	cout << (ans == INF ? "IMPOSSIBLE" : to_string(ans)) << '\n';
+string solve() {
+    int n;
+    cin >> n;
+
+    vector<point> points(n);
+    for (point& p : points) 
+        cin >> p;
+
+    point blue;
+    cin >> blue;
+
+    for (point& p : points) 
+        p -= blue;
+
+    vector<vector<double>> dist(n, vector<double>(n, INF + 10));
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            if (points[i] * points[j] > 0)
+                dist[i][j] = points[i].distance(points[j]);
+
+    for (int k = 0; k < n; ++k) 
+        for (int i = 0; i < n; ++i) 
+            for (int j = 0; j < n; ++j) 
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            
+    double ans = INF + 10;
+    for (int i = 0; i < n; ++i) 
+        ans = min(ans, dist[i][i]);
+
+    return (ans >= INF ? "IMPOSSIBLE" : to_string(ans));
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int T;
-	cin >> T;
-	for (int test_case = 1; test_case <= T; ++test_case) {
-		cout << "Case #" << test_case << ": ";
-		solve();
-	}
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    for (int test_case = 1; test_case <= T; ++test_case)
+        cout << "Case #" << test_case << ": " << solve() << '\n';
+    
+    return 0;
 }
