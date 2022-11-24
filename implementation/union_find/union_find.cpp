c9f2f08462e063f3bee4e81dc3d6076ee45384fe
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2022-11-24 19:05:46 +0000

    Improved DSU implementation

--- implementation/union_find/union_find.cpp
+++ implementation/union_find/union_find.cpp
@@ -1,57 +1,51 @@
 #include <bits/stdc++.h>
-using namespace std;
 
+/* Description: Disjoint Set Union data structure, with
+   path compression and union by size optimisations, to
+   allow for near constant time (amortized) queries. */
 struct union_find {
   private:
-	int cnt;
-	vector<int> sizes, link;
- 
+    int num_components;
+    std::vector<int> sizes, link;
+
   public:
-	union_find() {}
- 
-	union_find(int n) : cnt(n), sizes(n, 1), link(n, 0) { iota(link.begin(), link.end(), 0); }
- 
-	int find(int x) { return x == link[x] ? x : link[x] = find(link[x]); }
- 
-	bool same(int x, int y) { return find(x) == find(y); }
- 
-	void unite(int x, int y) {
-		x = find(x); y = find(y);
-		if (x == y) return;
-		if (sizes[x] < sizes[y]) 
-			swap(x, y);
-		
-		sizes[x] += sizes[y];
-		link[y] = x;
-		cnt--;
-	}
- 
-	int size(int x) { return sizes[find(x)]; }
- 
-	int count() const { return cnt; }	
+    union_find(int _num_components) : num_components(_num_components) {
+        sizes.assign(num_components, 1);
+        link.resize(num_components);
+        std::iota(link.begin(), link.end(), 0);
+    }
+
+    int count() const { return num_components; }
+    int size(int x) { return sizes[find(x)]; }
+    int find(int x) { return x == link[x] ? x : link[x] = find(link[x]); }
+
+    bool unite(int x, int y) {
+        x = find(x), y = find(y);
+        if (x == y) return false;
+        /* Ensure that y is a smaller set than x. */
+        if (sizes[x] < sizes[y]) std::swap(x, y);
+        /* Merge y into x. */
+        sizes[x] += sizes[y];
+        link[y] = x;
+        num_components--;
+        return true;
+    }
 };
 
-/*
-Example Main Program:
-(solution to https://atcoder.jp/contests/abc177/tasks/abc177_d)
-*/
+int main() { 
+    int n, m;
+    std::cin >> n >> m;
 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
- 
-	int n, m; 
-	cin >> n >> m;
-	union_find uf(n);
-	for (int i = 0; i < m; ++i) {
-		int a, b; 
-		cin >> a >> b; 
-		a--, b--;
-		uf.unite(a, b);
-	}
-	int ans = 0;
-	for (int i = 0; i < n; ++i) ans = max(ans, uf.size(i));
-	cout << ans << '\n';
- 
-	return 0;
+    union_find UF(n);
+    for (int i = 0, a, b; i < m; ++i) {
+        std::cin >> a >> b, a--, b--;
+        UF.unite(a, b);
+    }
+    
+    int ans = 0;
+    for (int i = 0; i < n; ++i) 
+        ans = std::max(ans, UF.size(i));
+
+    std::cout << ans << std::endl;
+    return 0;
 }
